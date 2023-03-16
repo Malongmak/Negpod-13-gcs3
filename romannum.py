8e5574d5658ae4b5722777d42740dabb7c514945
emmanuelnyadongo <e.nyadongo@gmail.com>
2023-03-16 19:08:56 +0200

    roman numeral

---
+++ romannum.py
@@ -0,0 +1,9 @@
+def roman_to_int(s: str) -> int:
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(s)):
+        if i > 0 and roman_dict[s[i]] > roman_dict[s[i - 1]]:
+            result += roman_dict[s[i]] - 2 * roman_dict[s[i - 1]]
+        else:
+            result += roman_dict[s[i]]
+    return result
